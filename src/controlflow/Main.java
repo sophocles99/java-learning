5dd66387446ed1f7d930b94107f38c04d32e8c5d
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-04 20:51:27 +0000

    more control flow examples

--- src/controlflow/Main.java
+++ src/controlflow/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Comparison and logical operators
         int x = 1;
         int y = 1;
         System.out.println(x == y); // Other comparison operators: !=  >  <  >=  <=
@@ -13,6 +14,7 @@ public class Main {
         boolean hasCriminalRecord = false;
         boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
 
+        // Switch example
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter your role: ");
         String role = scanner.next();
@@ -21,13 +23,29 @@ public class Main {
             case "admin":
                 System.out.println("You are an admin");
                 break;
-
             case "moderator":
                 System.out.println("You are a moderator");
                 break;
-
             default:
                 System.out.println("You are a guest");
         }
+
+        // for loop
+        for (int i = 0; i < 10; i++)
+            System.out.println((i + 1) + ". Java is the future..?");
+
+        // while loop
+        String input = "";
+        while (true) {
+            System.out.print("Enter something: ");
+            input = scanner.next().toLowerCase();
+            if (input.equals("quit")) break;
+            System.out.println("You entered: " + input);
+        }
+
+        // for each loop
+        String[] vehicles = {"car", "bus", "aeroplane", "ferry"};
+        for (String vehicle : vehicles)
+            System.out.println(vehicle);
     }
 }
