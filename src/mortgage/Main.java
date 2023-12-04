a79c52908b19f377457f34b84a948c7af9533b0b
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-04 21:50:27 +0000

    add range validation to inputs

--- src/mortgage/Main.java
+++ src/mortgage/Main.java
@@ -9,22 +9,43 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal;
+        float monthlyInterest;
+        int numberOfPayments;
+
         System.out.println("===========================");
         System.out.println("Mortgage Payment Calculator");
         System.out.println("===========================\n");
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Loan amount (£): ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Loan amount (£1000 - £1000,000): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Please enter a number between 1,000 and 1000,000.");
+        }
 
-        System.out.print("Annual interest rate (%): ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        while (true) {
+            System.out.print("Annual interest rate (%): ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+                break;
+            }
+            System.out.println("Please enter a number greater than 0 and less than or equal to 30.");
+        }
 
-        System.out.print("Duration of mortgage (duration): ");
-        int duration = scanner.nextInt();
-        int numberOfPayments = duration * 12;
+        while (true) {
+            System.out.print("Duration of mortgage (duration): ");
+            byte duration = scanner.nextByte();
+            if (duration >= 1 && duration <= 30) {
+                numberOfPayments = duration * 12;
+                break;
+            }
+            System.out.println("Please enter a number between 1 and 30.");
+        }
 
         double monthlyPayment = principal
                 * monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
