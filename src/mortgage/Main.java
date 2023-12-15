62a579df4129b952610f30915f2db74398f4f0d8
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-15 16:47:24 +0000

    tweak formatting

--- src/mortgage/Main.java
+++ src/mortgage/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
     final static byte MONTHS_IN_YEAR = 12;
     final static byte PERCENT = 100;
+
     public static void main(String[] args) {
         displayTitle();
 
@@ -27,18 +28,19 @@ public class Main {
     public static void displayMonthlyPayment(int principal, float annualInterest, byte years) {
         double monthlyPayment = calculateMonthlyPayment(principal, annualInterest, years);
         String monthlyPaymentString = NumberFormat.getCurrencyInstance(Locale.UK).format(monthlyPayment);
-        System.out.println("Monthly payments");
-        System.out.println("----------------");
-        System.out.println("\nMonthly payment: " + monthlyPaymentString);
+        System.out.println("Monthly payment");
+        System.out.println("---------------");
+        System.out.println(monthlyPaymentString + "\n");
     }
 
     public static void displayPaymentSchedule(int principal, float annualInterest, byte years) {
         System.out.println("Payment schedule");
         System.out.println("----------------");
+        System.out.println("Month\tBalance");
         for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
             double balance = calculateBalance(principal, annualInterest, years, month);
             String formattedBalance = NumberFormat.getCurrencyInstance(Locale.UK).format(balance);
-            System.out.println(String.format("%-5s", month + ".") + formattedBalance);
+            System.out.println(String.format("%-4s", month + ".") +"\t" + formattedBalance);
         }
     }
 
