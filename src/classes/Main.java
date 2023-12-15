cf1b5c1b82d3f86d1444ca4bb9167bcabe7bcdf0
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-15 16:47:45 +0000

    start Classes lesson

---
+++ src/classes/Main.java
@@ -0,0 +1,17 @@
+package classes;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
+    }
+
+}
