ebf3083f208967eb749f8e7e67ca845d4f7bacc7
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-15 17:22:12 +0000

    encapsulation example

---
+++ src/classes/Employee.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Employee {
+    public int baseSalary;
+    public int hourlyRate;
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+}
--- src/classes/Main.java
+++ src/classes/Main.java
@@ -3,15 +3,11 @@ package classes;
 import org.w3c.dom.ls.LSOutput;
 
 public class Main {
-
     public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
-
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
     }
-
 }
