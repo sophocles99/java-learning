bc88aa52bea6a2379a64da1784048e61a9660fc7
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-01-04 20:58:21 +0000

    use constructor to reduce coupling even further

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -4,14 +4,8 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        var browser = new Browser();
-
-//        var employee = new Employee();
-//
-//        employee.setBaseSalary(50_000);
-//        employee.setHourlyRate(20);
-//
-//        int wage = employee.calculateWage(10);
-//        System.out.println(wage);
+        var employee = new Employee(50_000, 20);
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
     }
 }
