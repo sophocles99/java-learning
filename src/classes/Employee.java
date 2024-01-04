90d13261892d364eba152c202cb5f2dcadfc0017
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-01-04 21:04:44 +0000

    demonstrate method overloading of calculateWage method

--- src/classes/Employee.java
+++ src/classes/Employee.java
@@ -13,6 +13,10 @@ public class Employee {
         return getBaseSalary() + (getHourlyRate() * extraHours);
     }
 
+    public int calculateWage() {
+        return calculateWage(0);
+    }
+
     private void setBaseSalary(int baseSalary) {
         if (baseSalary <= 0)
             throw new IllegalArgumentException("Salary cannot be 0 or less");
--- src/classes/Main.java
+++ src/classes/Main.java
@@ -5,7 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee(50_000, 20);
-        int wage = employee.calculateWage(10);
+        int wage = employee.calculateWage();
         System.out.println(wage);
     }
 }
