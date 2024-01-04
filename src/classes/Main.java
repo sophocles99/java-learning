de44080a1f78e16c75059e0c322af8600bcd91c1
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-01-04 21:27:58 +0000

    demonstrate static fields and methods

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -5,6 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee(50_000, 20);
+        Employee.printNumberOfEmployees();
         int wage = employee.calculateWage();
         System.out.println(wage);
     }
